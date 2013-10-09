97301f966655b1937298b69062657c38596f4460
Patrick Rose <pjr0911025+github@googlemail.com>
2013-10-09 11:12:27 +0100

    Fixed the seed file

--- app/database/seeds/UserTableSeed.php
+++ app/database/seeds/UserTableSeed.php
@@ -7,13 +7,15 @@ class UserTableSeeder extends Seeder {
         // Uncomment the below to wipe the table clean before populating
         DB::table('users')->truncate();
 
-        $users = array(
-	    array('username' => 'admin',
-		  'password' => Hash::make('changeMe'))
-        );
+        Eloquent::unguard();
+
+        User::create(array(
+            'username' => 'admin',
+            'password' => Hash::make('changeMe')
+        ));
 
         // Uncomment the below to run the seeder
-        DB::table('users')->insert($users);
+        //        DB::table('users')->insert($users);
     }
 
 }
